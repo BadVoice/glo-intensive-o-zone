d68c2fb47ce1c614ce43ac8b1c1e356fdfa19ab2
German Smurov <smurovnemez@mail.ru>
2022-05-17 12:34:07 +0300

    editing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import cart from "./modules/cart";
-
+import second from "./modules/second";
 cart()
+second()
--- src/modules/cart.js
+++ src/modules/cart.js
@@ -14,7 +14,6 @@ const cart = () => {
     cartBtn.addEventListener('click', openCart)
     cartCloseBtn.addEventListener('click', closeCart)
 
-    console.log(cartModal);
 }
 
 export default cart
